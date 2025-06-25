bbc81b3f2c9e6086efa8cdb6346b2ff5d71593c0
ferbarrera0312 <00115725@uca.edu.sv>
2025-06-24 18:26:13 -0600

    Funcionalidad de un jugador(Blackjack)

--- blackjack.cpp
+++ blackjack.cpp
@@ -23,6 +23,8 @@ Carta generarCarta()
 void jugarBlackjack()
 {
     srand(time(0));
+    suma = 0;
+    
     for (int i = 0; i < 2; i++)
     {
         Carta carta = generarCarta();
@@ -30,17 +32,50 @@ void jugarBlackjack()
 
         suma += carta.puntos;
     }
-    cout << "Tiene un puntaje de: " << suma << endl;
-    cout << "Desea tomar otra carta?" << endl;
-    cout << "1. Si" << endl;
-    cout << "0. No" << endl;
-    cin >> opcion;
 
-    if (opcion == 1)
+    while (suma < 21)
     {
-        Carta carta = generarCarta();
-        cout << "Carta obtenida: [" << carta.valor << ", valor: " << carta.puntos << "]" << endl;
-        suma += carta.puntos;
+        cout << "Tiene un puntaje de: " << suma << endl;
+
+        do
+        {
+            cout << "Desea tomar otra carta?" << endl;
+            cout << "1. Si" << endl;
+            cout << "0. No" << endl;
+            cin >> opcion;
+
+            if (opcion != 0 && opcion != 1)
+            {
+                cout << "Ingrese un numero valido" << endl;
+            }
+            else if (cin.fail())
+            {
+                cin.clear();
+                cin.ignore(1000, '\n');
+                opcion = -1;
+                cout << "Ingrese un valor valido" << endl;
+            }
+        } while (opcion != 0 && opcion != 1);
+
+        if (opcion == 1)
+        {
+            Carta carta = generarCarta();
+            cout << "Carta obtenida: [" << carta.valor << ", valor: " << carta.puntos << "]" << endl;
+            suma += carta.puntos;
+        }
+        else if (opcion == 0)
+        {
+            cout << "Te plantaste con un puntaje de: " << suma << endl;
+            break;
+        }
+
+        if (suma > 21)
+        {
+            cout << "PERDISTE!, Te pasaste de 21, tu puntaje fue de: " << suma << endl;
+        }
+        else if (suma == 21)
+        {
+            cout << "GANASTE!, obtuviste puntaje exacto";
+        }
     }
-    cout << "Tiene un puntaje de: " << suma << endl;
 }
