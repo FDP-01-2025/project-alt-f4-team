393f5addf6652d50db0e68deea6e68b3b3ca8977
francisca Guevara <00032125@uca.edu.sv>
2025-07-07 15:10:12 -0600

    jugador

--- veteapescar.cpp
+++ veteapescar.cpp
@@ -10,5 +10,10 @@ private:
     const static int tam_max = 26;
     const static int tam_baraja = 52;
     const char* valores[13] = {"a","2","3","4","5","6","7","8","9","10","j","q","k"};
-
+struct jugador {
+        char mano[tam_max][3];
+        int cantidad = 0;
+        int cuartetos = 0;
+        const char* nombre;
+    };
 };
