ca4e89d93e1ec5793f19813b22d4f5ad287322ad
francisca Guevara <00032125@uca.edu.sv>
2025-07-07 15:07:31 -0600

    variables iniciales

--- veteapescar.cpp
+++ veteapescar.cpp
@@ -8,4 +8,7 @@ class veteapescar {
 private:
     const static int num_jugadores = 3;
     const static int tam_max = 26;
+    const static int tam_baraja = 52;
+    const char* valores[13] = {"a","2","3","4","5","6","7","8","9","10","j","q","k"};
+
 };
