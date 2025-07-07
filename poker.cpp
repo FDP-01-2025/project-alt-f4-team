cad377516e301ad7a3c39ef78b09881130ba2ea3
Arana13N <00092725@uca.edu.sv>
2025-07-06 21:49:26 -0600

    evaluar jugada

--- poker.cpp
+++ poker.cpp
@@ -60,3 +60,59 @@ int contarvalor(Carta mano[5], const char* v) {
         if (strcmp(mano[i].valor, v) == 0) c++;
     return c;
 }
+
+
+int evaluarjugada(Carta mano[5], char resultado[30]) {
+    int valores[5];
+    int repeticiones[15] = {0};
+    int palos[4] = {0}; 
+
+    for (int i = 0; i < 5; i++) {
+        valores[i] = valornumerico(mano[i].valor);
+        repeticiones[valores[i]]++;
+
+        if (mano[i].palo == 'P') palos[0]++;
+        else if (mano[i].palo == 'C') palos[1]++;
+        else if (mano[i].palo == 'D') palos[2]++;
+        else if (mano[i].palo == 'T') palos[3]++;
+    }
+
+    for (int i = 0; i < 4; i++)
+        for (int j = i + 1; j < 5; j++)
+            if (valores[i] > valores[j]) {
+                int tmp = valores[i];
+                valores[i] = valores[j];
+                valores[j] = tmp;
+            }
+
+    bool escalera = true;
+    for (int i = 1; i < 5; i++)
+        if (valores[i] != valores[i - 1] + 1) escalera = false;
+
+    bool color = false;
+    for (int i = 0; i < 4; i++)
+        if (palos[i] == 5) color = true;
+
+    if (escalera && color) { strcpy(resultado, "Escalera de color"); return 9; }
+    for (int i = 2; i <= 14; i++)
+        if (repeticiones[i] == 4) { strcpy(resultado, "Póker"); return 8; }
+
+    bool tiene3 = false, tiene2 = false;
+    for (int i = 2; i <= 14; i++) {
+        if (repeticiones[i] == 3) tiene3 = true;
+        if (repeticiones[i] == 2) tiene2 = true;
+    }
+    if (tiene3 && tiene2) { strcpy(resultado, "Full house"); return 7; }
+    if (color) { strcpy(resultado, "Color"); return 6; }
+    if (escalera) { strcpy(resultado, "Escalera"); return 5; }
+    if (tiene3) { strcpy(resultado, "Trío"); return 4; }
+
+    int pares = 0;
+    for (int i = 2; i <= 14; i++)
+        if (repeticiones[i] == 2) pares++;
+    if (pares == 2) { strcpy(resultado, "Doble par"); return 3; }
+    if (pares == 1) { strcpy(resultado, "Par"); return 2; }
+
+    strcpy(resultado, "Carta alta");
+    return 1;
+}
